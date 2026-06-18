8985fb074f24ea68993fc1164c2d6a3756cbe77e
CNYJ256 <2553879@tongji.edu.cn>
2026-06-18 10:23:23 +0800

    更新

--- src/views/NavHomeView.vue
+++ src/views/NavHomeView.vue
@@ -63,11 +63,6 @@
       </div>
     </div>
   </div>
-  <img
-    :src="`${import.meta.env.BASE_URL}images/mascot.png`"
-    alt="看板娘"
-    class="fixed bottom-4 right-4 w-40 md:w-56 pointer-events-none z-10"
-  />
 </template>
 
 <script setup>
